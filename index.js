bc8063181ef5e63d2d59ba65c596b701b670a90d
CNova <caiocnova1@gmail.com>
2020-07-25 17:19:34 -0300

    Add nedb database connection

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const Datastore = require('nedb');
+
 const app = express();
 app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
+const database = new Datastore('database.db');
+database.loadDatabase();
+database.insert({name: 'Caio', status: 'true'});
+database.insert({name: 'Caio', status: 'true2'});
+
 app.post('/api', ( request, response ) => {
   console.log('Got a request!');
-  console.log(request.body);
+  const data = request.body;
+  database.insert(data);
+  console.log(database);
   response.json({
     status: 'success',
     latitude: request.body.lat,
